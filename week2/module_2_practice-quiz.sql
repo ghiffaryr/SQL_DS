75d8890fd799d9fe8ef7aefa370b938b1e70bea3
ghiffaryr <grifqialdi@gmail.com>
2021-09-01 14:52:39 +0700

    Week 4

--- week2/module_2_practice-quiz.sql
+++ week2/module_2_practice-quiz.sql
@@ -22,16 +22,16 @@ FROM salary_range_by_job_classification
 --5. Question 5
 What is the pay type for all the job codes that start with '03'? The code has been started for you, but you will need to program the fourth and fifth lines yourself before running the query.
 SELECT
-job_code,
-pay_type
+job_code
+,pay_type
 FROM salary_range_by_job_classification
 WHERE job_code LIKE '03%'
 
 --6. Run a query to find the Effective Date (eff_date) or Salary End Date (sal_end_date) for grade Q90H0? The code has been started for you, but you will need to program the third through the sixth lines yourself before running the query. 
 SELECT
-grade,
-eff_date,
-sal_end_date
+grade
+,eff_date
+,sal_end_date
 FROM salary_range_by_job_classification
 WHERE grade = 'Q90H0'
 
@@ -43,29 +43,29 @@ ORDER BY Biweekly_Low_Rate ASC
 
 --8. Write and run a query, with no starter code to answer this question: What Step are Job Codes 0110-0400? Hint: there are 6 lines to run this query.
 SELECT
-Step,
-Job_Code
+Step
+,Job_Code
 FROM salary_range_by_job_classification
 WHERE Job_Code BETWEEN '0110' AND '0400'
 
 --9. Write and run a query, with no starter code or hints to answer this question: What is the Biweekly High Rate minus the Biweekly Low Rate for job Code 0170?
 SELECT
-(Biweekly_High_Rate - Biweekly_Low_Rate) AS DIFF,
-Job_Code
+(Biweekly_High_Rate - Biweekly_Low_Rate) AS DIFF
+,Job_Code
 FROM salary_range_by_job_classification
 WHERE Job_Code = '0170'
 
 --10. Write and run a query, with no starter code or hints to answer this question: What is the Extended Step for Pay Types M, H, and D? 
 SELECT
-Extended_Step,
-Pay_Type
+Extended_Step
+,Pay_Type
 FROM salary_range_by_job_classification
 WHERE Pay_Type IN ('M', 'H', 'D')
 
 --11. Write and run a query, with no starter code or hints to answer this question: What is the step for Union Code 990 and a Set ID of SFMTA or COMMN? 
 SELECT
-Step,
-Union_Code,
-SetID
+Step
+,Union_Code
+,SetID
 FROM salary_range_by_job_classification
 WHERE Union_Code = '990'AND (SetID = 'SFMTA' OR SetID = 'COMMN')
--- week3/practice-quiz_writing-queries.sql
+++ week3/practice-quiz_writing-queries.sql
@@ -1,8 +1,8 @@
 --1. How many albums does the artist Led Zeppelin have? 
 SELECT
-COUNT(DISTINCT al.AlbumId) AS AlbumTotal, 
-al.ArtistId,
-ar.Name
+COUNT(DISTINCT al.AlbumId) AS AlbumTotal
+,al.ArtistId
+,ar.Name
 FROM albums al, artists ar
 WHERE  al.ArtistId in (SELECT
                         ar.ArtistId
@@ -11,25 +11,25 @@ WHERE  al.ArtistId in (SELECT
 
 --2. Create a list of album titles and the unit prices for the artist "Audioslave".
 SELECT
-al.Title,
-t.UnitPrice,
-ar.Name
+al.Title
+,t.UnitPrice
+,ar.Name
 FROM ((artists ar INNER JOIN albums al ON ar.ArtistId = al.ArtistId) 
                 INNER JOIN tracks t ON al.AlbumId = t.AlbumId)
 WHERE ar.Name = 'Audioslave'
 
 --3. Find the first and last name of any customer who does not have an invoice. Are there any customers returned from the query?
 SELECT
-c.FirstName,
-c.LastName,
-i.InvoiceId
+c.FirstName
+,c.LastName
+,i.InvoiceId
 FROM (customers c LEFT JOIN invoices i ON c.CustomerId = i.CustomerId)
 WHERE i.InvoiceId IS NULL
 
 --4. Find the total price for each album. What is the total price for the album “Big Ones”?
 SELECT
-al.Title,
-SUM(t.UnitPrice)
+al.Title
+,SUM(t.UnitPrice)
 FROM (albums al INNER JOIN tracks t ON al.AlbumId = t.AlbumId)
 WHERE al.Title = 'Big Ones'
 
